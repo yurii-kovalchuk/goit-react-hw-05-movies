f30eadf22efec087d22403e9bdf01f2e99a410ad
yurii-kovalchuk <sogdrums@gmail.com>
2023-02-10 17:06:45 +0200

    add cast component

---
+++ src/components/ActorCard/ActorCard.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const WrapAutoCard = styled.div`
+  margin-bottom: 5px;
+  padding: 5px;
+  border: 1px solid silver;
+`;
--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,3 +1,42 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ActorCard } from 'components/ActorCard/ActorCard';
+
 export const Cast = () => {
-  return <div>casts</div>;
+  const [cast, setCast] = useState(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchMovieCast = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=f9d814745439162b2afd5ff7833832ed`
+        );
+        setCast(response.data.cast);
+        console.dir(response.data.cast);
+      } catch (err) {
+        toast(err.message);
+      }
+    };
+
+    fetchMovieCast();
+  }, [movieId]);
+  return (
+    <div>
+      <ul>
+        {cast &&
+          cast.map(actor => (
+            <li key={actor.credit_id}>
+              <ActorCard
+                name={actor.name}
+                character={actor.character}
+                profile_path={actor.profile_path}
+              />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
